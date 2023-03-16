144b7081f6a213ed527a9c09269a133f1939c4f7
tharindaabeysena <tharindaabeysena@gmail.com>
2023-03-17 05:24:21 +0530

    add api request

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,8 +12,30 @@ class HomeScreen extends StatefulWidget {
 
 const String pokeApi =
     'https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json';
+List? pokedex;
 
 class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    if (mounted) {
+      fetchPokedexData();
+    }
+  }
+
+  void fetchPokedexData() {
+    var url = Uri.https('raw.githubusercontent.com',
+        '/Biuni/PokemonGO-Pokedex/master/pokedex.json');
+    http.get(url).then((value) {
+      if (value.statusCode == 200) {
+        var decodedJasonData = jsonDecode(value.body);
+        pokedex = decodedJasonData['pokemon'];
+      }
+      setState(() {});
+      print(pokedex![0]);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
